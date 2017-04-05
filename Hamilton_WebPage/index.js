cf7cf078d3d21d826a360bce70f614beec244140
Audrey <AAH75@pitt.edu>
2017-04-05 13:46:25 -0400

    Fixing main page on website

--- Hamilton_WebPage/index.js
+++ Hamilton_WebPage/index.js
@@ -5,7 +5,7 @@ function init() {
 
             
             function rotate() {
-          var images = new Array ('01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg')
+          var images = new Array ('01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg')
                 var thisImage = 0;
       var thisImage = Math.floor(Math.random()*(images.length)) ;
 
