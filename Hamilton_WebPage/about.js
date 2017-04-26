d8d0f471fc88fedd933e267213c965dff5975e9f
ebeshero <ebbondar@gmail.com>
2017-04-25 23:34:53 -0400

    repaired JavaScript for about.html page and small changes to about.html

--- Hamilton_WebPage/about.js
+++ Hamilton_WebPage/about.js
@@ -3,44 +3,21 @@ var about1 = document.getElementById("about1")
 var about2 = document.getElementById("about2")
 var about3 = document.getElementById("about3")
 function init() {
-var buttons = document.getElementsByTagName("button");
-buttons[0].addEventListener('click', toggleAbout1,false)
-buttons[1].addEventListener('click', toggleAbout2, false)
-buttons[2].addEventListener('click', toggleAbout3, false)
+var buttons = document.getElementsByTagName('button');
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', toggle, false);     
+}
+}
+function toggle() {
+window.alert("function lineShow is firing, and this.id is: " + this.id);
+    hideOthers()
+    var divID = 'about' + this.id
+    var selDiv = document.getElementById(divID)
+    selDiv.style.display= 'block';
 }
 
-function toggleAbout1() {
-    var about1 = document.getElementById("about1")
-        if(about1.style.display=='none') {
-            about1.style.display='block';
-            about2.style.display='none';
-            about3.style.display='none';
-        }
-        else {
-        about1.style.display='none';
-        }
-}
-
-function toggleAbout2() {
-    var about2 = document.getElementById("about2")
-        if(about2.style.display=='none') {
-            about2.style.display='block';
-            about1.style.display='none';
-            about3.style.display='none';
-        }
-        else {
-        about2.style.display='none';
-        }
-}
-
-function toggleAbout3() {
-    var about3 = document.getElementById("about3")
-        if(about3.style.display=='none') {
-            about3.style.display='block';
-            about2.style.display='none';
-            about1.style.display='none';
-        }
-        else {
-        about3.style.display='none';
-        }
+function hideOthers() {
+var divs = document.getElementsByClassName('main')
+for (var d = 0; d < divs.length; d++) 
+divs[d].style.display='none';
 }
