43a326134331fede711664c29fcc5b4be974b6f1
Rebecca Parker <rjp43@pitt.edu>
2017-04-27 15:11:31 -0400

    again attempting to fix javaScript on second photo reel

--- Hamilton_WebPage/index.js
+++ Hamilton_WebPage/index.js
@@ -14,13 +14,13 @@ function init() {
          setTimeout(rotate, 8 * 1000);
           }
            function rotate2() {
-          var images = new Array ('17.jpg', '18.jpg', '19.jpg', '20.jpg', '21.jpg', '22.jpg', '23.jpg', '24.jpg', '25.jpg', '26.jpg', '27.jpg', '28.jpg', '29.jpg', '30.jpg', '31.jpg', '32.jpg')
+          var images2 = new Array ('17.jpg', '18.jpg', '19.jpg', '20.jpg', '21.jpg', '22.jpg', '23.jpg', '24.jpg', '25.jpg', '26.jpg', '27.jpg', '28.jpg', '29.jpg', '30.jpg', '31.jpg', '32.jpg')
                 var thisImage = 0;
-      var thisImage = Math.floor(Math.random()*(images.length)) ;
+      var thisImage = Math.floor(Math.random()*(images2.length)) ;
 
-          document.getElementById("rotator2").src = images[thisImage];
+          document.getElementById("rotator2").src = images2[thisImage];
 
-         setTimeout(rotate, 8 * 1000);
+         setTimeout(rotate2, 8 * 1000);
           }
 
        
